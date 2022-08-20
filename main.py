06ff73da743e5288444a03ff46e5465a02a3434e
praneetk77 <praneetkarna77@gmail.com>
2022-08-21 03:14:54 +0530

    Trained and saved the model

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(pattern)
+        docs_x.append(wrds)
         docs_y.append(intent["tag"])
 
     if(intent["tag"] not in labels):
         labels.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
@@ -60,3 +60,14 @@ training = np.array(training)
 output = np.array(output)
 
 
+net = tflearn.input_data(shape=[None,len(training[0])])
+net = tflearn.fully_connected(net,8)
+net = tflearn.fully_connected(net,8)
+net = tflearn.fully_connected(net,len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output,n_epoch=1000, batch_size=8, show_metric=True)
+
+model.save("model.tflearn")
